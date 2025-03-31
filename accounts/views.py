13df1beeb5bf90ae9e49d4892e2587e5fdba14e4
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-31 22:22:27 +0100

    Working in geister view and url

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
 
-# Create your views here.
+def register_view(request):
+    if request.method=='GET':
+        return render(request, 'register.html')
+    elif request.method=='POST':
+        name = request.POST.get('username')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+
+        if not password == password_confirm:
+            return redirect('register')
+        
+        if len(password):
+            return redirect('register')
+        
+        users = User.objects.filter(username=name)
+
+        if users.exists():
+            return redirect('register')
+        
+        user = User.objects.create_user(
+            username=name,
+            password=password
+        )
+        login(request, user)
+
+        return redirect('home')
