9573451a0ebe95afc2661b1e206c74e2ffa5487b
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-31 22:44:36 +0100

    Authenticate the user

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def register_view(request):
     if request.method=='GET':
@@ -11,20 +12,30 @@ def register_view(request):
         password_confirm = request.POST.get('password_confirm')
 
         if not password == password_confirm:
+            messages.error(request, 'Password diferentes. Tente novamente.')
             return redirect('register')
         
         if len(password):
+            messages.error(request, 'A senha deve ter ao menos 6 caracteres.')
+            return redirect('register')
+
+        if User.objects.filter(username=name).exists():
             return redirect('register')
         
-        users = User.objects.filter(username=name)
+        try:
+            User.objects.create_user(
+                username=name,
+                password=password
+            )
 
-        if users.exists():
+            user = authenticate(request, username=name, password=password)
+            if user:
+                login(request, user)
+                messages.success(request, 'Usuário criado com sucesso.')
+                return redirect('home')
+            else:
+                messages.error(request, 'Erro ao autenticar usuário.')
+                return redirect('home')
+        except Exception as err:
+            messages.error(request, f'Erro ao criar usuário: {str(err)}')
             return redirect('register')
-        
-        user = User.objects.create_user(
-            username=name,
-            password=password
-        )
-        login(request, user)
-
-        return redirect('home')
