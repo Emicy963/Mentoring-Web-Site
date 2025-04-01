a04f86ab8c0a384d92820231a4c13398bf635df1
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-01 22:37:01 +0100

    Working in models

--- mentees/models.py
+++ mentees/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Navigators(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Mentees(models.Model):
+    choice_stage = (
+        ('E1', '10-100k'),
+        ('E2', '100-1kk')
+    )
+    name = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='photos', null=True, blank=True)
+    stage = models.CharField(max_length=2, choices=choice_stage)
+    navigator = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
