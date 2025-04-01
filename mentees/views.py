7601b02f8253f4aa78886f2e293100a4c2ceec2a
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-01 23:14:08 +0100

    Working in form for create a mentee

--- mentees/views.py
+++ mentees/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Navigators, Mentees
+from django.contrib import messages
+from django.contrib.messages import constants
 
-# Create your views here.
+def mentees_views(request):
+    if request.method=='GET':
+        navigators = Navigators.objects.filter(user=request.user)
+        mentees = Mentees.objects.filter(user=request.user)
+        return render(request, 'mentees.html', {'stages': Mentees.choice_stage, 'navigators': navigators, 'mentees': mentees})
+    elif request.method=='POST':
+        name = request.POST.get('name')
+        photo = request.POST.get('photo')
+        stage = request.POST.get('stage')
+        navigator = request.POST.get('navigator')
+        try:
+            mentees = Mentees(
+                name=name,
+                photo=photo,
+                stage=stage,
+                navigator=navigator,
+                user=request.user
+            )
+
+            mentees.save()
+
+            messages.add_message(request, constants.SUCCESS, 'Mentorado cadastrado com sucesso!')
+            return redirect('mentees')
+        except Exception as err:
+            messages.add_message(request, constants.ERROR, f'Erro ao cadastrar mentorado: {str(err)}')
+            return redirect('mentees')
