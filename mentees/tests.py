8b49cfc82595400753a649b6999eabd701b72667
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-02 23:50:44 +0100

    Working in unittest for testing if is everthing ok with database and create objects

--- mentees/tests.py
+++ mentees/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+import pytest
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Navigators, Mentees
 
-# Create your tests here.
+class MenteesTestCase(TestCase):
+    def test_navigators_model(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        navigator = Navigators.objects.create(name='Navigator 1', user=user)
+        self.assertEqual(str(navigator), 'Navigator 1')
+
+    def test_mentees_model(self):
+        user = User.objects.create_user(username='testuser', password='12345')
+        navigator = Navigators.objects.create(name='Navigator 1', user=user)
+        mentee = Mentees.objects.create(name='Mentee 1', stage='E1', navigator=navigator, user=user)
+        self.assertEqual(str(mentee), 'Mentee 1')
