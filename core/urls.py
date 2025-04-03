e4119714634dc8276c92078926641707cc8c7fb4
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-03 21:36:06 +0100

    Render the views to template

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('mentees/', include('mentees.urls')),
+    path('meetings/', include('meentigns.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
