abd80333985dd5d649ff0a4da742c343ef028d8c
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-04-04 23:05:21 +0100

    Create a token field for save the mentees token

--- mentees/models.py
+++ mentees/models.py
@@ -1,3 +1,4 @@
+import secrets
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -19,6 +20,18 @@ class Mentees(models.Model):
     navigator = models.ForeignKey(Navigators, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     create_at = models.DateField(auto_now_add=True)
+    token = models.CharField(max_length=16, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = self.generate_unique_token()
+        super().save(*args, **kwargs)
+
+    def generate_unique_token(self):
+        while True:
+            token = secrets.token_urlsafe(8)
+            if not Mentees.objects.filter(token=token).exists():
+                return token
 
     def __str__(self):
         return self.name
